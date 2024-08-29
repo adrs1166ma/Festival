93d0e0567e307e3a1445fe01d9a46423abe77a4f
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-08-29 01:51:34 -0500

    script dev added

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-import {src, dest} from 'gulp'
+import {src, dest, watch} from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
@@ -9,4 +9,9 @@ export function css( done ) {
         .pipe(sass())
         .pipe(dest('build/css'))
     done()
+}
+
+export function dev() {
+    watch('src/scss/app.scss', css)
+
 }
