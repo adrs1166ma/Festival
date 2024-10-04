4b492ef8d5aded47dd1792db3c931e8c8d0baefe
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-10-04 11:42:33 -0500

    Galeria con js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
-alert('Hola mundo!!!!!!!!')
+document.addEventListener('DOMContentLoaded', function() {
+    crearGaleria()
+})
+
+function crearGaleria() {
+
+    const CANTIDAD_IMAGENES = 16;
+    const galeria = document.querySelector('.galeria-imagenes')
+
+    for(let i = 1; i<= CANTIDAD_IMAGENES; i++){
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg`
+        imagen.alt = 'Imagen Galería'
+        console.log(imagen)
+
+        galeria.appendChild(imagen)
+    }
+}
