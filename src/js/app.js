40173a46fc7ee811bc5897132cf74c7a3ddc3c25
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-10-05 11:55:17 -0500

    se creo modal

--- src/js/app.js
+++ src/js/app.js
@@ -11,8 +11,34 @@ function crearGaleria() {
         const imagen = document.createElement('IMG')
         imagen.src = `src/img/gallery/full/${i}.jpg`
         imagen.alt = 'Imagen Galería'
-        console.log(imagen)
+        
+        
+        // Event Handler
+        imagen.onclick = function() {
+            mostrarImagen(i)
+        }
 
         galeria.appendChild(imagen)
     }
+}
+
+function mostrarImagen(i) {
+    
+    // Generar Modal
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+
+    // Agregar al html
+    const body = document.querySelector('body')
+    body.appendChild(modal)
+
+    console.log(modal)
+
+}
+
+function cerrarModal() {
+    const modal = document.querySelector('.modal')
+
+    modal?.remove()
 }
